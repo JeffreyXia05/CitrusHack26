06835c5a4546f7ec88e80e435ed5598bab6305f5
Yilin Liu <yilinliu741@gmail.com>
2026-04-19 01:02:24 -0700

    added weird walking animation toggle

--- states.py
+++ states.py
@@ -56,9 +56,9 @@ def load_states(option, settings):
         get_frames(row=52, frame_count=4) * 3 + get_frames(row=0, frame_count=1) * 4
     ]
     # -------------------------
-    # WALK (8 directions)
+    # WALK (8 directions) and (spinning walk)
     # -------------------------
-    walk_frames = {
+    normal_walk = {
         "down": get_frames(row=4, frame_count=4),
         "down_left": get_frames(row=8, frame_count=6),
         "left": get_frames(row=7, frame_count=8),
@@ -68,6 +68,19 @@ def load_states(option, settings):
         "right": get_frames(row=6, frame_count=6),
         "down_right": get_frames(row=9, frame_count=6),
     }
+
+    weird_walk = {
+        "down": get_frames(row=1, frame_count=8),
+        "down_left": get_frames(row=1, frame_count=8),
+        "left": get_frames(row=1, frame_count=8),
+        "up_left": get_frames(row=1, frame_count=8),
+        "up": get_frames(row=1, frame_count=8),
+        "up_right": get_frames(row=1, frame_count=8),
+        "right": get_frames(row=1, frame_count=8),
+        "down_right": get_frames(row=1, frame_count=8),
+    }
+
+    walk_frames = weird_walk if settings.get("weirdWalk", True) else normal_walk
 
     # -------------------------
     # SLEEP
